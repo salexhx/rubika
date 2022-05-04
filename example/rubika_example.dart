f31d18f45d1fe7624904cc67e889b391f6d2c84d
Saleh <71204370+salexhx@users.noreply.github.com>
2022-05-04 13:30:28 +0430

    Update rubika_example.dart

--- example/rubika_example.dart
+++ example/rubika_example.dart
@@ -3,4 +3,5 @@ import 'package:rubika/rubika.dart' as rubika;
 void main() {
   rubika.client Bot = new rubika.client("Auth");
   await Bot.sendMessage("text", "target");
+  print(rubika.encryption.decode("Data", "Auth"));
 }
